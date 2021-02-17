879ae6ccd46b36365503ae5b885457d824ca5a58
Dakauann <74578069+Dakauann@users.noreply.github.com>
2021-02-17 02:05:10 -0300

    Update badut.sh

--- badut.sh
+++ badut.sh
@@ -4,7 +4,7 @@ hijau="\033[1;32m"
 putih="\033[1;37m"
 biru="\033[0;34m"
 kuning="\033[1;33m"
-paku="darkzin"
+paku="trash"
 until [ "$pass" = "$paku" ]
 do
 clear
@@ -22,13 +22,13 @@ echo "          |   ..     ..  |"
 echo "          |     $merah----- $putih   |"
 echo "           ==============" $merah
 echo "     ____________"
-echo " ___| Ferramenta By __| " $putih
-echo "|  Dark Yt__|"
+echo " ___| Ferramenta feita por __| " $putih
+echo "|  TrashDk__|"
 echo "|__________| $merah"
 echo "                     _________________________"
 echo "                    | Ferramenta de login 1•  |"
 echo "                     ^^^^^^^^^^^^^^^^^^^^^^^^^"
-read -p "insira a senha da ferramenta : " pass
+read -p "insira a senha de verificação : " pass
 echo "Checando senha, aguarde....."
 sleep 3
 done
@@ -47,22 +47,22 @@ echo "          |   ..     ..  |"
 echo "          |     $merah----- $putih   |"
 echo "           ==============" $merah
 echo "     ____________"
-echo " ___| Ferramenta By __| " $putih
-echo "|  Dark Yt__|"
+echo " ___| Ferramenta feita por __| " $putih
+echo "|  TrashDk__|"
 echo "|__________|"
 echo "$hijau"
 echo "     ______         ______"
-echo "    |      | Darkbot |    |"
+echo "    |      | TrashDkbot |    |"
 echo "     |___________________|" $kuning
 echo " Nota : $putih Com esta ferramenta, você"
-echo "         pode fazer o bot Whatsapp facilmente,"
+echo "         pode fazer o bot Whatsapp facinho mano,"
 echo "          sem o incômodo, de colocar comandos"
 read -p "Você quer continuar?[y/n] " lanjut
 if [ "$lanjut" = "n" ]
 then
 sleep 2
-echo "ok mana, a instalação do bot pode ser feita em outro"
-echo "hoje tenha uma boa atividade mana....."
+echo "ok mano, a instalação do bot pode ser feita em outro"
+echo "dia mano,tenha um bom dia....."
 sleep 3
 elif [ "$lanjut" = "y" ]
 then
@@ -73,10 +73,10 @@ clear
 sleep 2
 echo "          $merah   instalando o segundo login"
 sleep 2
-git clone https://github.com/Darkziinh/SITEX
-git clone https://github.com/Darkziinh/apikey
-git clone https://github.com/Darkziinh/testek
-cd darkbot3.0
+git clone https://github.com/Dakauann/verifica-o-1
+git clone https://github.com/Dakauann/verificar2
+git clone https://github.com/Dakauann/verifica-o-1
+cd TrashDkbot4.0
 chmod +x git.sh
 echo "$verde"
 echo "Agora tem 2 etapas de senhas...."
